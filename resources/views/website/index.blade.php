48b7e04dcfcf424036665a0de81cdc4f9eb50487
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-01-29 17:23:18 -0600

    Home 80%
    Hace falta crear el formulario de información

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -26,8 +26,6 @@
         </div>
     </div>
 
-
-
     <div class="row text-center categorias">
 
         <div class="col-3 p-0">
@@ -44,8 +42,7 @@
         </div>
     </div>
 
-
-    <div class="row tablas-carrusel mb-5">
+    <div class="row tablas-carrusel">
         <div class="col-6 ml-auto">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
@@ -160,17 +157,14 @@
         </div>
 
         <div class="col-5 text-center texto-carrusel-tablas">
-            <h2>Tablas en tendencia</h2>
+            <h2>Tablas más vendidas</h2>
             <p class="col-8 mx-auto mt-5 text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Aperiam a quos consequuntur eligendi commodi neque laboriosam minus explicabo adipisci deserunt!</p>
             <button class="boton-portada mt-4">Ver más</button>
         </div>
-
-
     </div>
 
     <div class="row carrusel-noticias">
-
         <div id="carouselExampleIndicators" class="carousel slide col-9 mx-auto" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -200,6 +194,304 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </button>
+        </div>
+    </div>
+
+    <div class="row ropa-carrusel mb-5">
+        <div class="col-5 text-center texto-carrusel-tablas">
+            <h2>Playeras favoritas</h2>
+            <p class="col-8 mx-auto mt-5 text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aperiam a quos consequuntur eligendi commodi neque laboriosam minus explicabo adipisci deserunt!</p>
+            <button class="boton-portada mt-4">Ver más</button>
+        </div>
+
+        <div class="col-6 mr-auto">
+            <div id="carouselClothesControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla11.png" class="card-img-top img-tabla-carrusel text-start" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quiche card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla12.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quf the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla13.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quf the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla17.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somelk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla9.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somef the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla10.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Som the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla18.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quik of the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla16.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somee bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla6.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Sf the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla8.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some q of the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla8.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somthe card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla2.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somee card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <a class="carousel-control-prev" href="#carouselClothesControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselClothesControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row tendencias-carrusel mb-5">
+
+        <div class="col-8 mx-auto">
+
+            <h2 class="text-center mb-5">Tendencias</h2>
+            <div id="carouselTendenciaControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla11.png" class="card-img-top img-tabla-carrusel text-start" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quiche card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla12.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quf the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla13.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quf the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla9.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somef the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla10.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Som the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla18.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quik of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="cards-wrapper">
+                            <div class="card">
+                                <img src="images/tablas/tabla6.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Sf the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla8.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some q of the card's content.</p>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="images/tablas/tabla8.png" class="card-img-top img-tabla-carrusel" alt="...">
+                                <div class="card-body text-center p-0">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Somthe card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <a class="carousel-control-prev" href="#carouselCTendenciaControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselTendenciaControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row text-center blog ">
+
+        <div class="col-4 ml-auto p-0">
+            <img src="images/tablaCuadros.jpg" alt="" class="img-blog">
+            <h3 class="btn-portada">Blog</h3>
+        </div>
+        <div class="col-4 mr-auto  p-0">
+            <img src="images/skating.jpg" alt="" class="img-blog">
+            <h3>Eventos</h3>
+        </div>
+    </div>
+
+    <div class="row marcas-carrusel mb-5">
+        <div class="col-8 mx-auto">
+
+            <h2 class="text-center mb-5">Marcas disponibles</h2>
+            <div id="carouselBrandsControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="cards-wrapper">
+                            <div class="card mx-auto">
+                                <img src="images/logos/antifashion.png" class="card-img-top img-marca-carrusel mt-5" alt="...">
+                            </div>
+                            <div class="card ml-auto">
+                                <img src="images/logos/element.png" class="img-marca-carrusel mt-5" alt="...">
+                            </div>
+                            <div class="card">
+                                <img src="images/logos/RnD.png" class=" img-marca-carrusel" alt="...">
+                            </div>
+                            <div class="card">
+                                <img src="images/logos/dc.png" class=" img-marca-carrusel mt-5" alt="...">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item ">
+                        <div class="cards-wrapper">
+                            <div class="card mx-auto">
+                                <img src="images/logos/chocolate.png" class="card-img-top img-marca-carrusel " alt="...">
+                            </div>
+                            <div class="card ml-auto">
+                                <img src="images/logos/toyMachine.png" class="element img-marca-carrusel" alt="...">
+                            </div>
+                            <div class="card">
+                                <img src="images/logos/santaCruz.png" class="card-img-top img-marca-carrusel" alt="...">
+                            </div>
+                            <div class="card">
+                                <img src="images/logos/spitFire.png" class=" img-marca-carrusel mt-5" alt="...">
+                            </div>
+                        </div>
+                    </div>
+
+                    <a class="carousel-control-prev" href="#carouselBrandsControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselBrandsControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row contacto-form">
+        <div class="col-4 mx-auto ">
+            <h4 class="d-inline text-center">Contactanos</h4>
+            <p>Te responderemos lo más pronto posible :)</p>
+
+
+
+        </div>
+
+        <div class="col-6">
+
         </div>
 
     </div>
