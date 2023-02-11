f3d1f162892608b86090a26cc816550133788a32
Damzepher <115036592+Damzepher@users.noreply.github.com>
2023-02-10 22:54:13 -0600

    sidebar con entrada al create y data de clientes

--- resources/views/admin/plantilla/sidebar.blade.php
+++ resources/views/admin/plantilla/sidebar.blade.php
@@ -8,7 +8,8 @@
                 <li class="text-left"><a href="{{ url('/inventarioRopa') }}" class="sidebarli"> Ropa </a></li>
                 <li class="text-left"><a href="{{ url('/inventarioTenis') }}" class="sidebarli"> Tenis </a></li>
                 <li class="text-left"><a href="{{ url('/inventarioTablas') }}" class="sidebarli"> Tablas </a></li>
-                <li class="text-left"><a href="" class="sidebarli"> Clientes </a></li>
+                <li class="text-left"><a href="{{ url('/registroClientes') }}" class="sidebarli">Registro de Clientes </a></li>
+                <li class="text-left"><a href="{{ url('/inventarioClientes') }}" class="sidebarli">Clientes </a></li>
                 <li class="text-left"><a href="" class="sidebarli"> Equipo </a></li>
             </ul>
         </div>
