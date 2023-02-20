cdc70e442cc118393cb46cf668bf3f1d52f67f1b
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-02-20 10:21:54 -0600

    Modelos de las tablas "nota, evento y carruseles"

---
+++ app/Http/Controllers/CarruselMarcaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\carruselMarca;
+use App\Http\Requests\StorecarruselMarcaRequest;
+use App\Http\Requests\UpdatecarruselMarcaRequest;
+
+class CarruselMarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorecarruselMarcaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorecarruselMarcaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\carruselMarca  $carruselMarca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(carruselMarca $carruselMarca)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\carruselMarca  $carruselMarca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(carruselMarca $carruselMarca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatecarruselMarcaRequest  $request
+     * @param  \App\Models\carruselMarca  $carruselMarca
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatecarruselMarcaRequest $request, carruselMarca $carruselMarca)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\carruselMarca  $carruselMarca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(carruselMarca $carruselMarca)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CarruselNoticiaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\carruselNoticia;
+use App\Http\Requests\StorecarruselNoticiaRequest;
+use App\Http\Requests\UpdatecarruselNoticiaRequest;
+
+class CarruselNoticiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorecarruselNoticiaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorecarruselNoticiaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\carruselNoticia  $carruselNoticia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(carruselNoticia $carruselNoticia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\carruselNoticia  $carruselNoticia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(carruselNoticia $carruselNoticia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatecarruselNoticiaRequest  $request
+     * @param  \App\Models\carruselNoticia  $carruselNoticia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatecarruselNoticiaRequest $request, carruselNoticia $carruselNoticia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\carruselNoticia  $carruselNoticia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(carruselNoticia $carruselNoticia)
+    {
+        //
+    }
+}
