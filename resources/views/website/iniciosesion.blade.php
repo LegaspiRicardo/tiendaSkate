2b99930c557ce7c9ae20bb792adc1d8e1caf3944
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-02-20 23:54:51 -0600

    Inicio de Sesion completado

--- resources/views/website/iniciosesion.blade.php
+++ resources/views/website/iniciosesion.blade.php
@@ -1,53 +1,58 @@
-
- @extends('website.piezas.plantilla')
- <link href="css/signin.css" rel="stylesheet">
+@extends('website.piezas.plantilla')
+<link href="css/signin.css" rel="stylesheet">
 @section('titulo', 'Tienda de Skate Cool')
 
 @section('main')
 
 
-      
-  <main class="main-cuenta">
-    <img src="/images/grafiti1.jpg" alt="" class="imagen-login-fondo">
 
-    
-    <h1 class="titulo"> Inicia Sesión</h1>
-
-    <div class="caja-inicio-sesion">
-
-          <div class="caja-foto">
-              <div class="caja-formulario-cuenta">
-
-                    <div class="flex-column">
-                      <label class="label-formulario" for="correo">Tu correo</label>
-                      <input class="input-formulario" id="correo" type="email" name="correo" placeholder="dudas@gmail.com">
-
-                    </div>
-                    <div  class="flex-column">
-                      <label class="label-formulario" for="contraseña">Tu contraseña</label>
-                      <input class="input-formulario" id="contraseña" type="password" name="contraseña" placeholder="tu contraseña">
-
-                    </div>
-                    <div class="botones-cuenta botones-cuenta-sesion">
-                      <button class="btn-selector">Acceder</button>
-                      <a href="{{ url('crearcuenta') }}">¿No tienes cuenta? Crea una aquí</a>
-                    </div>
+<main class="main-cuenta">
+  <img src="/images/grafiti1.jpg" alt="" class="imagen-login-fondo">
 
 
-              </div>
 
-              
+  <div class="caja-inicio-sesion">
 
-          </div>
+
+
+
+    <div class="caja-foto">
+      <div class="caja-formulario-cuenta">
+
+      <h2 class="titulo"> Inicia sesión</h2>
+        <div class="flex-column">
+          <label class="label-formulario" for="correo">Correo:</label>
+          <input class="input-formulario" id="correo" type="email" name="correo">
+
+        </div>
+        <div class="flex-column">
+          <label class="label-formulario" for="contraseña">Contraseña:</label>
+          <input class="input-formulario" id="contraseña" type="password" name="contraseña">
+
+        </div>
+
+
+        <div class="botones-cuenta botones-cuenta-sesion">
+          <button class="btn-acceder ">Acceder</button>
+          <br>
+          <a href="{{ url('crearcuenta') }}">¿No tienes cuenta? Crea una aquí</a>
+        </div>
+
+
+      </div>
+
+
 
     </div>
 
+  </div>
 
-  </main>
+
+</main>
 
 
- 
-        
+
+
 
 
 
