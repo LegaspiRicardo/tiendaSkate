fb9c1d5806e2c3843711472e50ff2f0889c38cca
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-02-28 08:32:21 -0600

    Agregué tabla en alumno y modifique modal

---
+++ resources/views/admin/productos/data.blade.php
@@ -0,0 +1,160 @@
+<!--Table-->
+<table class="table table-hover table-sm">
+
+    <!--Table head-->
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Modelo</th>
+            <th>Marca</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th>Color</th>
+            <th>Material</th>
+            <th>Tamaño</th>
+            <th>Estatus</th>
+            <th>Descripción</th>
+            <th>Imagen</th>
+            <th>Editar</th>
+            <th>Borrar</th>
+        </tr>
+    </thead>
+    <!--Table head-->
+
+    <!--Table body-->
+    <tbody>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td>Jerry</td>
+            <td>Horwitz</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+            <td>Italy</td>
+            <td>Bari</td>
+            <td>Editor-in-chief</td>
+            <td>41</td>
+        </tr>
+    </tbody>
+    <!--Table body-->
+
+</table>
+<!--Table-->
--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -6,7 +6,10 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
 
     @include ('admin.layout.forms.productoForm')
-    @section ('contenidoBoton', 'producto')
+
+    <div class="tablaProductos">
+        @include ('admin.productos.data')
+    </div>
 
 </main>
 
