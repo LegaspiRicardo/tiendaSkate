300a1e95995953a616321c9520b1418df7782b03
Itzel Villaseñor <110121112+Itzelvillg@users.noreply.github.com>
2023-03-06 23:03:07 -0600

    Add files via upload

--- migrations/2023_02_16_040736_create_empleados_table.php
+++ migrations/2023_02_16_040736_create_empleados_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->integer('telefono')->nullable();
             $table->string('estatus')->nullable();
             $table->string('puesto');
+            $table->string('img1', 255);
             $table->timestamps();
         });
     }
