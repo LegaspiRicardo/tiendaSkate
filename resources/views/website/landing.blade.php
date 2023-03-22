cd2a5902e297b003f087beda14fe073dbd4f1e10
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-03-22 12:39:57 -0600

    Landin Page con Gif prron

---
+++ resources/views/website/landing.blade.php
@@ -0,0 +1,37 @@
+@extends('website.piezas.plantillaDash')
+
+@section('titulo', 'Radical Boards Landing Page')
+
+@section('main')
+<img class="imagen-fondo-landing" src="images/skatePark3.jpg" alt="">
+
+<div class="container--flex">
+
+    <h2 class="titulo-landing">¡Deslízate hacia la libertad! <p class="text-center">Visita nuestra tienda en línea y sé parte de la comunidad   </p></h2>
+
+    <img src="images/skaterGif.gif" class="gif" alt="">
+
+    <div class="row col-8 mx-auto divisor-landing mb-4"></div>
+
+
+<div class="row p-0 m-0">
+    <div class="col-6 mx-auto">
+        <div class="col-10 mx-auto text-center">
+            <h6 class="text-center">Encuentra todo lo que necesitas para patinar</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum excepturi quia eligendi modi eius id!</p>
+        </div>
+    </div>
+    <div class="col-6 mx-auto">
+        <div class="col-10 mx-auto text-center">
+            <h6>Titulo para ver donde ando</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum excepturi quia eligendi modi eius id!</p>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+</div>
+@endsection
